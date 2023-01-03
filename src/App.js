cfd422737a1e06cf1a1ff0ac7b2b91487aa25e9b
patrick akpala <patrickakpala@192.168.1.184>
2023-01-03 19:50:44 +0000

    First Commit
    Added prettier config and pre-commit hook
    Added budget form fields

--- src/App.js
+++ src/App.js
@@ -5,19 +5,89 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Zero Based Budgeting App
       </header>
+      <main>
+        <form>
+          <div className="bdgt-header-container">
+            <div className="bdgt-total-income-hero">
+              £ 0.00
+            </div>
+            <div className="bdgt-fields">
+              <div className="bdgt-primary-fields">
+                <label for="salary">Salary 1</label>
+                <input id="salary" type="text" name="salary_1" value="" />
+              </div>
+              <div className="bdgt-secondary-fields">
+                <label for="additionalIncome">Salary 2</label>
+                <input id="additonalIncome" type="text" name="salary_2" value="" />
+              </div>
+            </div>
+          </div>
+          <div className="bdgt-group-container">
+            <div className="bdgt-group-header"><h3>Savings</h3></div>
+            <div className="bdgt-group-items">
+              <div>
+                <label>Emergency Fund: </label>
+                <input type="text" name="savings_emergency_fund" value="" />
+              </div>
+              <div>
+                <h4>Planned</h4>
+                <p>£0.00</p>
+              </div>
+              <div>
+                <h4>Remaining</h4>
+                <p>£0.00</p>
+              </div>
+            </div>
+            <div className="bdgt-group-items-actions">
+              <button>Add New Group Item</button>
+            </div>
+          </div>
+          <div className="bdgt-group-container">
+            <div className="bdgt-group-header"><h3>Housing</h3></div>
+            <div className="bdgt-group-items">
+              <div>
+                <label>Rent: </label>
+                <input type="text" name="housing_rent" value="" />
+              </div>
+              <div>
+                <h4>Planned</h4>
+                <p>£0.00</p>
+              </div>
+              <div>
+                <h4>Remaining</h4>
+                <p>£0.00</p>
+              </div>
+            </div>
+            <div className="bdgt-group-items-actions">
+              <button>Add New Group Item</button>
+            </div>
+          </div>
+          <div className="bdgt-group-container">
+            <div className="bdgt-group-header"><h3>Transport</h3></div>
+            <div className="bdgt-group-items">
+              <div>
+                <label>TFL: </label>
+                <input type="text" name="transport_tfl" value="" />
+              </div>
+              <div>
+                <h4>Planned</h4>
+                <p>£0.00</p>
+              </div>
+              <div>
+                <h4>Remaining</h4>
+                <p>£0.00</p>
+              </div>
+            </div>
+            <div className="bdgt-group-items-actions">
+              <button>Add New Group Item</button>
+            </div>
+          </div>
+        </form>
+      </main>
+      <footer>
+      </footer>
     </div>
   );
 }
