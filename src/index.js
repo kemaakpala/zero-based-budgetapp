c5e5200b92299332ef4a60179916007c4eefde5e
kema akpala <kemaakpala@hotmail.com>
2023-05-13 02:57:04 +0100

    Update normalise
    deleted normalize file and instead installed it via npm.
    updated imports to reflect this change.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './normalize.css';
+import 'normalize.css';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import App from './App';
